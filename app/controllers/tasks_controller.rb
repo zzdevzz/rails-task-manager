5e45c977f513cec1e2c1964116f95c698e65b02c
dev <dev.h@hotmail.co.uk>
2024-04-30 23:36:05 +0100

    partial included and refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  # this before_action method will run the function 'set_task' on the actions below so code is more DRY.
+
   def index
     @tasks = Task.all
   end
 
   def show
     id = params[:id]
-    @task = Task.find(id)
   end
 
   def new
@@ -19,17 +21,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to tasks_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.delete
     redirect_to tasks_path status: :see_other
   end
@@ -40,5 +39,9 @@ class TasksController < ApplicationController
   # Prevents other inputs from hackers messing up database with new fields.
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
